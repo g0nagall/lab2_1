653269205cbaacee64a63387868b8af23c482ca0
g0nagall <50554188+g0nagall@users.noreply.github.com>
2019-05-28 02:50:28 +0300

    Add files via upload

---
+++ lab2_1/lab2_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab2_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			double g = 9.8;
+			Console.WriteLine("Enter m");
+			int m = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter h");
+			int h = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter v");
+			int v = int.Parse(Console.ReadLine());
+			Console.WriteLine("E = " + (m * v * v) / 2);
+			Console.WriteLine("P = " + (m * g * h));
+			Console.ReadKey();
+		}
+	}
+}
+
